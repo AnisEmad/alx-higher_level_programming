55d8d6bb4defe26c9676adfb04f75b8fd83805c1
Anis Emad <anisemad123@gmail.com>
2023-08-28 11:02:53 +0300

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    index = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                index = index + 1
+        print("")
+        return index
+    except:
+        print(" IndexError: list index out of range")
+        return index
