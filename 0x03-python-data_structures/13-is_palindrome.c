62b82939121c8f8e29d6374b0e374fd724ca3cd7
Anis Emad <anisemad123@gmail.com>
2023-08-29 13:00:00 +0300

    fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,49 +8,49 @@
 
 listint_t *add_nodeint_begin(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-    if (*head != NULL)
-        new->next = *head;
-    *head = new;
-    return (new);
+	if (*head != NULL)
+		new->next = *head;
+	*head = new;
+	return (new);
 }
 /*
  * is_palindrome - checks if a list is plaindrome or not
  * @head: pointer to pointer to the first element
- * 
+ *
  * Return: 1 if true, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *new_list;
-    listint_t *temp;
-    int comp = 1;
+	listint_t *new_list;
+	listint_t *temp;
+	int comp = 1;
 
-    temp = *head;
+	temp = *head;
 
-    while (temp != NULL)
-    {
-        add_nodeint_begin(&new_list, temp->n);
-        temp = temp->next;
-    }
-    temp = *head;
-    while (temp != NULL)
-    {
-        if (temp->n != new_list->n)
-        {
-            comp = 0;
-            break;
-        }
-        temp = temp->next;
-        new_list = new_list->next;
-    }
-    return (comp);
+	while (temp != NULL)
+	{
+		add_nodeint_begin(&new_list, temp->n);
+		temp = temp->next;
+	}
+	temp = *head;
+	while (temp != NULL)
+	{
+		if (temp->n != new_list->n)
+		{
+			comp = 0;
+			break;
+		}
+		temp = temp->next;
+		new_list = new_list->next;
+	}
+	return (comp);
 }
