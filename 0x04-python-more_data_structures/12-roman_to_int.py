ef2e77ba267cd9e72c225b8454f59b6406c317cb
Anis Emad <anisemad123@gmail.com>
2023-08-30 15:53:51 +0300

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+def check_input(string):
+    for i in string:
+        if i not in 'MCDLXVI':
+            return False
+    return True
+
+
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    if not check_input(roman_string):
+        return 0
     dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
             "D": 500, "M": 1000}
     num = 0
-    
     for i in range(len(roman_string)):
         if i + 1 < len(roman_string):
             if dict[roman_string[i]] < dict[roman_string[i + 1]]:
