1199d1f026be0e10ca954ca8c6c0c9c2b5b89a8a
Anis Emad <anisemad123@gmail.com>
2023-09-14 18:34:19 +0300

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+""" defining a to_json_string function"""
+
+
+def to_json_string(my_obj):
+    """ returns a json representation of an object"""
+    json_string = json.dumps(my_obj)
+    return json_string
