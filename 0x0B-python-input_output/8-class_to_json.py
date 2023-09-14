cf9f74981915c103a45a7d8f31432242315f5eef
Anis Emad <anisemad123@gmail.com>
2023-09-14 19:23:56 +0300

    change task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,11 +4,4 @@
 
 def class_to_json(obj):
     """ class to json fucntion """
-    if isinstance(obj, dict):
-        return {key: class_to_json(value) for key, value in obj.items()}
-    elif isinstance(obj, list):
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, (str, int, bool)):
-        return obj
-    else:
-        return str(obj)
+    return obj.__dict__
