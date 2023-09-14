5f372ad948f76987f0d741b14068422384e80208
Anis Emad <anisemad123@gmail.com>
2023-09-14 18:21:05 +0300

    fix task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """ append text to filename"""
     with open(filename, mode="a", encoding="utf-8") as file:
-        return file.append(text)
+        return file.write(text)
