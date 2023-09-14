16f1b4b1e7d1e8e6f2c4856969c6505c26ad337a
Anis Emad <anisemad123@gmail.com>
2023-09-14 19:27:19 +0300

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" this module is for student class"""
+
+
+class student:
+    """ calss student with attributes"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        return self.__dict__
