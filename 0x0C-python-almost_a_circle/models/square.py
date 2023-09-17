e4b288cde8f2a5851b79b0a5c1365ce9784eea10
Anis Emad <anisemad123@gmail.com>
2023-09-17 21:51:08 +0300

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" defining a square class """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        str = f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return str
+
+    def update(self, *args, **kwargs):
+        if args:
+            n = len(args)
+            if n > 0:
+                self.id = args[0]
+            if n > 1:
+                self.size = args[1]
+            if n > 2:
+                self.x = args[2]
+            if n > 3:
+                self.y = args[3]
+        elif kwargs:
+            for key, item in kwargs.items():
+                if key == "id":
+                    self.id = item
+                elif key == "size":
+                    self.size = item
+                elif key == "x":
+                    self.x = item
+                elif key == "y":
+                    self.y = item
