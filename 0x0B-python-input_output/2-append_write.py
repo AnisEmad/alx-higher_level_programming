0a139408e7293680efc3176a92c702aa7fe4aec6
AnisEmad <58657706+AnisEmad@users.noreply.github.com>
2023-10-02 13:29:58 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" defining a append_write function"""
-
-
-def append_write(filename="", text=""):
-    """ append text to filename"""
-    with open(filename, mode="a", encoding="utf-8") as file:
-        return file.write(text)
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" defining a class_to_json function"""
-
-
-def class_to_json(obj):
-    """ class to json fucntion """
-    return obj.__dict__
