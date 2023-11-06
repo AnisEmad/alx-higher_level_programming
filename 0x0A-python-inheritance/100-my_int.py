1bdf7a889c2c89684b2088b50045d79e3102b7a5
Anis Emad <anisemad123@gmail.com>
2023-11-06 22:49:48 +0200

    fix

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,5 +6,6 @@ class MyInt(int):
     """ calss myint that inherit from int """
     def __eq__(self, other):
         return super().__ne__(other)
+
     def __ne__(self, other):
         return super().__eq__(other)
