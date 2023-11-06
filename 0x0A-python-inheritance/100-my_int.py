56372760a5dab8e977c28bd3ea81d6c76da458e0
Anis Emad <anisemad123@gmail.com>
2023-11-06 22:48:58 +0200

    task advanced

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this module for my_int class """
+
+
+class MyInt(int):
+    """ calss myint that inherit from int """
+    def __eq__(self, other):
+        return super().__ne__(other)
+    def __ne__(self, other):
+        return super().__eq__(other)
