fbd4fe09ef6ec3b4c66524be3cb9afe54aec44a8
Anis Emad <anisemad123@gmail.com>
2023-11-06 17:47:55 +0200

    add

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    if not hasattr(magic_string, "n"): magic_string.n = 0; magic_string+= 1
-    return magic_string.n * "BestSchool"
+    if not hasattr(magic_string, "n"): magic_string.n = 0
+    return (count := count + 1)
