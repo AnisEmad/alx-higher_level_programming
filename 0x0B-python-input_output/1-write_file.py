f3ba83630c90a4cc2dc8b15fccd47d44a85cc7b6
Anis Emad <anisemad123@gmail.com>
2023-11-07 15:07:50 +0200

    fixed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """ this function write to a file and return the number of bytes written"""
     with open(filename, 'w') as f:
         return f.write(text)
