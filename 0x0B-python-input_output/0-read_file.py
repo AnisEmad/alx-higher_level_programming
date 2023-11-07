0bf00435f09faf0ac4002823666fbf2a9d0f8fed
Anis Emad <anisemad123@gmail.com>
2023-11-07 15:12:21 +0200

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module for read_file function """
+
+
+def read_file(filename=""):
+    """ this function reads a file and print it"""
+    with open(filename, 'r') as f:
+        file = f.read()
+        print(file)
