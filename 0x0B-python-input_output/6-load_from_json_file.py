abf359fdc8a5a872ea326be43ee3aca6f236a356
Anis Emad <anisemad123@gmail.com>
2023-11-07 15:43:50 +0200

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module for load_from_json_file function """
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ this function loads object form json file """
+    with open(filename, 'r') as file:
+        return json.load(filename)
