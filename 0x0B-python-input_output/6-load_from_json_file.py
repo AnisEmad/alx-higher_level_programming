4f49e004985b97455631e077905cac44af2e6471
Anis Emad <anisemad123@gmail.com>
2023-11-07 15:46:35 +0200

    style issue

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json_file(filename):
     """ this function loads object form json file """
     with open(filename, 'r') as file:
-            json_data = json.load(file)
-            return json_data
+        json_data = json.load(file)
+        return json_data
