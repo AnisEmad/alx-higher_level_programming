0d3468dee091e2af89001ade60f6cb199459f470
Anis Emad <anisemad123@gmail.com>
2023-11-07 15:19:45 +0200

    fixed style issue

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,9 @@
 
 
 def append_write(filename="", text=""):
-    """ this function append text to a file and creates one if it doesn't exist """
+    """
+    this function append text to a file and
+    creates one if it doesn't exist 
+    """
     with open(filename, 'a') as file:
         return file.write(text)
