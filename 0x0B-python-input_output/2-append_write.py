3fd6784e7f5d7459a38be5a671ce9d0ae08569b1
Anis Emad <anisemad123@gmail.com>
2023-11-07 15:18:33 +0200

    added documentation to task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,6 @@
 
 
 def append_write(filename="", text=""):
+    """ this function append text to a file and creates one if it doesn't exist """
     with open(filename, 'a') as file:
         return file.write(text)
