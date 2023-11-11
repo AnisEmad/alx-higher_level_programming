76a7d4c5ed3ae6912fe874ea089d54781c2c68ad
Anis Emad <anisemad123@gmail.com>
2023-11-11 14:10:20 +0200

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,11 @@
 """ this module for add_item script """
 
 
+import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
 
 
 args = sys.argv
