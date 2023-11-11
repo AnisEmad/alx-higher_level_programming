56fba319139df7698bff61e53af86e08c660712f
Anis Emad <anisemad123@gmail.com>
2023-11-11 15:42:16 +0200

    added a to_dictionry to sqaure

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,3 +35,11 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+            "id" : self.id,
+            "size" : self.size,
+            "x" : self.x,
+            "y" : self.y,
+        }
