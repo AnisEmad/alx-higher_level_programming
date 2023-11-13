1ecdaf7753d1f297a077535b1880108f82cc25df
Anis Emad <anisemad123@gmail.com>
2023-11-13 19:46:28 +0200

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,6 +10,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ constructor """
         if id is not None:
             self.id = id
         else:
@@ -17,16 +18,19 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_dictionary(self):
+        """ to dictonary function """
         pass
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ this convert list of dicts to a json string """
         if list_dictionaries is None or len(list_dictionaries) <= 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """ this saves a list of objs to a file """
         if list_objs is None:
             data = []
         
@@ -40,18 +44,21 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """ this convert json string to an object """
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+        """ this function creates an instance with attriubtes """
         dummy = cls(1, 1)
         dummy.update(**dictionary)
         return dummy    
 
     @classmethod
     def load_from_file(cls):
+        """ this function loads a list of obj from a file """
         try:
             filename = f"{cls.__name__}.json"
             with open(filename, 'r') as file:
